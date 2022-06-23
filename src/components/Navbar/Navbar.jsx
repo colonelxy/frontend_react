138f5a95d0879f3cd2db0732d94b91627e8d20f4
colonelxy <harold2@protonmail.com>
2022-06-23 13:17:40 +0300

    created navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
-
+import { images } from '../../constants';
 import './Navbar.scss';
+
+
 
 const Navbar = () => {
   return (
-    <nav>
-        <div>
+    <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
             <img src={images.logo} alt='logo' />
         </div>
+        <ul className='app__navbar-links'>
+            { ['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li  classname='app__flex p-text' key={'link-${item'}>
+                    <div />
+                    <a href={'#${item}'}>{item}</a>
+                </li>
+            ) )}
+
+        </ul>
     </nav>
   )
 }
