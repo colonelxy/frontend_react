b7465e2a7c59e1d3bf0ae813f4c6dbce69ddd7bf
colonelxy <harold2@protonmail.com>
2022-06-28 12:57:01 +0300

    Edited header.scss

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -67,7 +67,7 @@ const Header = () => {
       variant={scaleVariants}
       whileInView = {scaleVariants.whileInView}
       className ='app__header-circles'>
-        {[images.python, images.react, images.aws].map((circle, index) =>(
+        {[images.python, images.react, images.aws, images.web3].map((circle, index) =>(
           <div className='circle-cmp app__flex' key={`circle-${index}`}>
             <img src={circle} alt='circle'/>
 
