3d25c3d9b75b3bf730c4c972d9caa4f0fac95c44
colonelxy <harold2@protonmail.com>
2022-06-28 10:25:44 +0300

    Added header contents

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
+import { images } from '../../constants';
 import './Header.scss';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='app__header app__flex'>
+      <motion.div
+        whileInView={{ x: [-100,0], opacity: [0,1]}}
+        transition={{ duration: 0.5}}
+        className='app__header-info'
+      >
+        <div className='app__header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>👋</span>
+            <div style={{ marginLeft: 20}}>
+              <p className='p-text'> Hello, I am</p>
+              <h1 className='head-text'>Harold</h1>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </motion.div>
+
+    </div>
   )
 }
 
